b39700f59053f27defa6ad19f914c04fdd573786
irfan-files <vfx55782@gmail.com>
2024-10-30 21:44:53 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let name = "test123";
+    let age = 27;
+    let amount: i64 = 50004923759235234;
+
+    println!(
+        "{} is {} years old and has {} in their account",
+        name, age, amount
+    );
+}
