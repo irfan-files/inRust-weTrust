1f1293cfcaeae9a1ef7434589b4ccd5159f5fb3e
irfan-files <vfx55782@gmail.com>
2024-11-10 03:16:27 +0700

    for loop another mechanism

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
-use rand::Rng;
+
 
 #[allow(unused_variables)]
 #[allow(unused_assignments)]
 
 
 fn main() {
-    let mut rng = rand::thread_rng();
-    let num = rng.gen_range(0,11);
-    if num >= 5 {
-        println!("{} is even", num);
-    } else {
-        println!("{} is odd", num);
+    for i  in 1..=11{
+      println!("{0} * {0} = {1}",i,i*i);
     }
-}
+
+    let pets = ["cat","dog","fish","bird","snake"];
+    for pet in pets.iter(){
+
+      if pet == &"snake"{
+        println!("{}, I don't like snakes", pet);
+        continue;
+      }
+      if pet == &"fish"{
+        println!("{}, I it's a pet", pet);
+        continue;
+          
+      }
+
+        println!("{pet}");
+    }
+
+    for (pos,i) in (1..11).enumerate(){
+      let square = i*i;
+      let nb = pos+1;
+      println!("{0} * {0} = {1}", nb,square);
+    }
+  }
