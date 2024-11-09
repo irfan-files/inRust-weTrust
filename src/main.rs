bc9b9c9dd08698e27bd70e16a261d634fce12052
irfan-files <vfx55782@gmail.com>
2024-11-10 02:34:41 +0700

    if statement

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
-use rand::distributions::Alphanumeric;
-use rand::{thread_rng, Rng};
+use rand::Rng;
+
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+
 
 fn main() {
     let mut rng = rand::thread_rng();
-    let i: i32 = rng.gen();
-    println!("{}", i);
-
-    println!("{}", rand::thread_rng().gen_range(0,100));
-    println!("{}", rand::thread_rng().gen_range(0.0,100.0));
-
-    let rand_string: String = thread_rng()
-    .sample_iter(Alphanumeric)
-    .take(30)
-    .collect();
-    println!("{}", rand_string);
+    let num = rng.gen_range(0,11);
+    if num >= 5 {
+        println!("{} is even", num);
+    } else {
+        println!("{} is odd", num);
+    }
 }
