f322d423d2ea49efa0b94c18f78ff3ce9683d732
irfan-files <vfx55782@gmail.com>
2024-11-12 16:03:29 +0700

    lambda operation

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,13 @@
 
 
 fn main() {
-    for i  in 1..=11{
-      println!("{0} * {0} = {1}",i,i*i);
+   let a = |a:i32| a+1;
+println!("{}",a(5));
+let b = |b:i32| -> i32 {
+  let c = b+1;
+    c
+};
+println!("{}",b(5));
+
     }
-
-    let pets = ["cat","dog","fish","bird","snake"];
-    for pet in pets.iter(){
-
-      if pet == &"snake"{
-        println!("{}, I don't like snakes", pet);
-        continue;
-      }
-      if pet == &"fish"{
-        println!("{}, I it's a pet", pet);
-        continue;
-          
-      }
-
-        println!("{pet}");
-    }
-
-    for (pos,i) in (1..11).enumerate(){
-      let square = i*i;
-      let nb = pos+1;
-      println!("{0} * {0} = {1}", nb,square);
-    }
-  }
+  
